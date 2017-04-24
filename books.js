806f2e14db020c3986613d284174460312d2e69f
teimilola <root@localhost.localdomain>
2017-04-24 19:05:27 -0400

    Added ajax request to list categories in books.js

---
+++ books.js
@@ -0,0 +1,45 @@
+//Javascript program fetches list of books categories via ajax
+//And displays the books under the category selected
+
+/*jslint devel: true */
+/*jslint white: true */
+/*global $, window */
+/*jslint browser: true */
+/*jslint this: true */
+/*jslint for: true */
+
+(function() {
+    "use strict";
+
+    function ajaxError (ajax, exception) { 
+        console.log("Error making ajax request: " + "\nStatus: " + ajax.status + "  "
+          + ajax.statusText + "\nResponse: " + ajax.responseText); 
+    }
+
+    function displayCategory() {
+        var type = "list_category";
+        $.ajax({
+            url: "booklist.php",
+            type: "POST",
+            crossOrigin: true,
+            data: {type: type},
+            success: function (response) {
+                console.log(response);
+            },
+            error: ajaxError
+        });
+    }
+
+    function displayInCategory($category) {
+        var type = "in_category";
+    }
+    //Window load function
+    window.onload=function () {
+        //display available categories on load
+        displayCategory();
+        //On select categories,display books in category
+        $("list_books").addEventListener("click", function () {
+            displayInCategory($('#category input[name=choice]:checked').val());
+        });
+    };
+}());
---
+++ books.php
@@ -0,0 +1,4 @@
+<?php
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script type="text/javascript" src="books.js"></script>
