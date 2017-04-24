33feeef261b405382c2cb780e3984e76c4dcdec5
teimilola <root@localhost.localdomain>
2017-04-24 17:53:08 -0400

    Added basic booklist logic

---
+++ booklist.php
@@ -0,0 +1,42 @@
+<?php
+
+function sanitize_input($string=""){
+    $string = htmlspecialchars($string);
+    $string = trim($string);
+    $string = stripslashes($string);
+}
+
+function fetchQuery($db, $sql){
+    try {
+        $smt = $db->prepare($sql);
+        $smt->execute();
+        $rows = $smt->fetchAll();
+        return $rows;
+     } catch (PDOException $ex) {
+        error_log($ex->getMessage(), 3, "errors.txt");
+     }
+}
+
+if($SERVER['REQUEST_METHOD'] == 'POST' ){
+    $db = new PDO("mysql:dbnamebook;host=localhost", "root");
+    $db->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $type = sanitize_input($_REQUEST['type']);
+    
+    if($type == "list_category") {
+        $sql = "SELECT * FROM category";
+        $rows = fetchQuery($db, $sql);
+        var_dump($rows);
+    }
+    if ($type === "in_category") {
+        $category = $db->quote($_REQUEST['category']);
+        $sql = "SELECT t.id, t.name, y.published, p.amount, c.name, a.name
+            FROM title t JOIN year y ON y.id = t.id
+            JOIN price p ON p.id = t.id
+            JOIN category c ON c.id = t.id
+            JOIN author a ON a.id = t.id WHERE c.name <> $category";
+        $rows = fetchQuery($db, $sql);
+        var_dump($rows);
+    }
+    
+}
+?>
