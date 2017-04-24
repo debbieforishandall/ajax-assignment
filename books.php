715ff51864bf3ebe65cdb09578a8b4a4b2f1843f
teimilola <root@localhost.localdomain>
2017-04-24 19:09:30 -0400

    Added button and div to books.php

--- books.php
+++ books.php
@@ -2,3 +2,9 @@
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script type="text/javascript" src="books.js"></script>
+<html>
+<body>
+    <div id="category"></div>
+    <button id="list_books"></button>
+</body>
+</html>
