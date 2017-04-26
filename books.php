af9aa0e1c0dd332d024340af8bf40ad449568dba
teimilola <root@localhost.localdomain>
2017-04-26 00:00:19 -0400

    Made categories and buttons inline

--- books.php
+++ books.php
@@ -3,6 +3,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
 <script type="text/javascript" src="books.js"></script>
 <html>
+<head>
+    <link rel="stylesheet" href="books.css">
+</head>
 <body>
     <div id="category"></div>
     <button id="list_books">List Books</button>
