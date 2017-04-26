50fee73204342255725d115707770ef16a8cb85a
teimilola <root@localhost.localdomain>
2017-04-25 23:54:41 -0400

    Fixed errors in json list categories

--- books.js
+++ books.js
@@ -30,7 +30,7 @@
                     var id = category[i].getElementsByTagName("id")[0].firstChild.nodeValue;
                     var name = category[i].getElementsByTagName("name")[0].firstChild.nodeValue;
                     //make a radio button containing category
-                    var radioHtml = "<input type='radio' name='choice' id='" + id + "'>" + name;
+                    var radioHtml = "<input type='radio' name='choice' id='" + id + "' value='" + name +"'>" + name;
                     $("category").appendChild(radioHtml);
                 }
             },
@@ -48,11 +48,22 @@
             onSuccess: function (ajax) {
                 console.log(ajax);
                 var data = JSON.parse(ajax.responseText);
+                console.log("data" + data[1].name);
                 for (var i = 0; i < data.length; i++) {
                     var name = data[i].name;
                     //make a radio button containing category
-                    var radioHtml = "<input type='radio' name='choice' id='" + i + "'>" + name;
-                    $("category").appendChild(radioHtml);      
+                    //Work around for IE notl etting you set the names of radio buttons dynamically
+
+                    var radioHtml = "<input type='radio' name='choice' id='" + i + "' value='" 
++ name +"'>" + name;
+                    console.log(radioHtml);
+                    var radioFragment = document.createElement('div');
+                    radioFragment.innerHTML = radioHtml;
+                    var labelHtml = "<label for='" + i + "'>"+name+"</label>";
+                    var labelFragment = document.createElement('div');
+                    labelFragment.innerHTML = labelHtml;
+                    $("category").appendChild(radioFragment.firstChild);   
+                    $("category").appendChild(labelFragment.firstChild); 
                 }
             },
             onFailure: ajaxFailure
